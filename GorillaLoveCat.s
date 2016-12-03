9a43b91347886ef104901cea1a6164dfcd3fdb55
Suyie Z <apple@wirelessprv-10-192-83-81.near.illinois.edu>
2016-12-02 20:50:46 -0600

    不知道叫什么

--- GorillaLoveCat.s
+++ GorillaLoveCat.s
@@ -523,15 +523,6 @@ clone_for_loop_end:
 
 main:
 
-############ USE OF REGISTERS ############
-
-	# s0 = water resource
-	# s1 = seed resource
-	# s2 = fire resource
-	# s3 = tile_data TILE_SCAN
-	# s4 = x
-	# s5 = y
-
 ############ ENABLE INTERRUPTS ############
 
 	# enable interrupts
@@ -550,6 +541,15 @@ start:
 	sw	$s3,	TILE_SCAN
 
 main_loop:
+
+############ USE OF REGISTERS ############
+
+	# s0 = water resource
+	# s1 = seed resource
+	# s2 = fire resource
+	# s3 = tile_data TILE_SCAN
+	# s4 = x
+	# s5 = y
 
 ############ CHECK RESOURCES ############
 
@@ -614,6 +614,7 @@ state_0:
 	beq	$s1,	0,	finish_action
 action_plant:
 	sw	$0,	SEED_TILE
+	sub	$s1,	$s1,	1
 	j	finish_action
 
 state_1:
@@ -624,12 +625,14 @@ my_plant:
 action_water:
 	li	$t0,	10	# Dump 10 units of water
 	sw	$t0,	WATER_TILE
+	sub	$s0,	$s0,	1
 	j	finish_action
 
 others_plant:
 	beq	$s2,	0,	finish_action
 action_fire:
 	sw	$0,	BURN_TILE
+	sub	$s2,	$s2,	1
 	j	finish_action
 
 finish_action:
@@ -673,7 +676,7 @@ x_increase:
 	li	$t0,	180
 	sw	$t0,	ANGLE
 	li	$t0,	1
-	sw	$t2,	ANGLE_CONTROL
+	sw	$t0,	ANGLE_CONTROL
 	add	$t0,	$s4,	1
 x_increase_loop:
 	lw	$s4,	BOT_X
@@ -686,7 +689,7 @@ x_decrease:
 	li	$t0,	0
 	sw	$t0,	ANGLE
 	li	$t0,	1
-	sw	$t2,	ANGLE_CONTROL
+	sw	$t0,	ANGLE_CONTROL
 	sub	$t0,	$s4,	1
 x_decrease_loop:
 	lw	$s4,	BOT_X
@@ -699,7 +702,7 @@ y_increase:
 	li	$t0,	270
 	sw	$t0,	ANGLE
 	li	$t0,	1
-	sw	$t2,	ANGLE_CONTROL
+	sw	$t0,	ANGLE_CONTROL
 	add	$t0,	$s5,	1
 y_increase_loop:
 	lw	$s5,	BOT_Y
@@ -712,7 +715,7 @@ y_decrease:
 	li	$t0,	90
 	sw	$t0,	ANGLE
 	li	$t0,	1
-	sw	$t2,	ANGLE_CONTROL
+	sw	$t0,	ANGLE_CONTROL
 	sub	$t0,	$s5,	1
 y_decrease_loop:
 	lw	$s5,	BOT_Y
@@ -772,11 +775,85 @@ interrupt_dispatch:			# Interrupt:
 	li	$v0,	PRINT_STRING	# Unhandled interrupt types
 	la	$a0,	unhandled_str
 	syscall
-	j		done
+	j	done
 
 ############ HANDLE INTERRUPTS ############
 
 fire_interrupt:
+
+	sw	$a1,	ON_FIRE_ACK	# acknowledge interrupt
+	lw	$t8,	GET_FIRE_LOC
+	and 	$t7,	$t8,	0xffff
+	mul		$t7,	$t7,	30
+	add 	$t7,	$t7,	15
+	srl		$t8,	$t8,	16
+	mul		$t8,	$t8,	30
+	add 	$t8,	$t8,	15
+
+	li 		$t2,	1 			# t2 = 1
+	li	 	$t3,	2
+	sw		$t3,	VELOCITY	# VELOCITY = 2
+
+	li 		$t3,	0
+	li 		$t4,	90
+	li 		$t5,	180
+	li 		$t6,	270
+
+find:
+	lw		$t9,	BOT_X
+	beq		$t9,	$t8,	x_equal
+	bgt		$t9, 	$t8,	x_big
+	j 		x_small
+
+x_big:
+	sw		$t5,	ANGLE
+	sw		$t2,	ANGLE_CONTROL
+x_big_loop:
+	lw		$t9,	BOT_X
+	blt		$t9,	0xf,	x_equal
+	ble		$t9,	$t8,	x_equal
+	j 		x_big_loop
+
+x_small:
+	sw		$t3,	ANGLE
+	sw		$t2,	ANGLE_CONTROL
+x_small_loop:
+	lw		$t9,	BOT_X
+	bgt		$t9,	0x11d,	x_equal
+	bge		$t9,	$t8,	x_equal
+	j 		x_small_loop
+
+x_equal:
+
+	lw 		$t9,	BOT_Y		# t9 = y
+	beq		$t9,	$t7,	y_equal
+	bgt		$t9, 	$t7,	y_big
+	j 		y_small
+
+y_big:
+	sw		$t6,	ANGLE
+	sw		$t2,	ANGLE_CONTROL
+y_big_loop:
+	lw		$t9,	BOT_Y
+	blt		$t9,	0xf,	y_equal
+	ble		$t9,	$t7,	y_equal
+	j 		y_big_loop
+
+y_small:
+	sw		$t4,	ANGLE
+	sw		$t2,	ANGLE_CONTROL
+y_small_loop:
+	lw		$t9,	BOT_Y
+	bgt		$t9,	0x11d,	y_equal
+	bge		$t9,	$t7,	y_equal
+	j 		y_small_loop
+
+y_equal:
+
+	sw		$t2,	PUT_OUT_FIRE
+	sw		$0,		VELOCITY		# drive
+
+	j		interrupt_dispatch
 
 max_growth_interrupt:
 
@@ -784,17 +861,17 @@ request_puzzle_interrupt:
 
 ############ END OF INTERRUPTS ############
 
-non_intrpt:				# was some non-interrupt
-	li	$v0, PRINT_STRING
-	la	$a0, non_intrpt_str
-	syscall				# print out an error message
+non_intrpt:	# was some non-interrupt
+	li	$v0,	PRINT_STRING
+	la	$a0,	non_intrpt_str
+	syscall	# print out an error message
 	# fall through to done
 
 done:
 	la	$k0,	chunkIH
-	lw	$a0,	0($k0)		# Restore saved registers
+	lw	$a0,	0($k0)	# Restore saved registers
 	lw	$a1,	4($k0)
 .set noat
-	move	$at,	$k1		# Restore $at
+	move	$at,	$k1	# Restore $at
 .set at
 	eret
